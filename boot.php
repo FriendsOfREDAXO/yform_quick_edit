b02d104e505c921341a581b3d0cfaa336ff7d5c4
eaCe <webmaster@slaps.de>
2022-01-23 16:24:43 +0100

    feat: add csrf token (yform >= 4)

--- boot.php
+++ boot.php
@@ -21,12 +21,30 @@ if (\rex::isBackend() && \rex::getUser()) {
         \rex_view::addJsFile($addon->getAssetsUrl('js/scripts.js'));
     }
 
-    \rex_extension::register('YFORM_DATA_LIST', function ($ep) {
+    \rex_extension::register('YFORM_DATA_LIST', function (\rex_extension_point $ep) {
         /** @var rex_list $list */
         $list = $ep->getSubject();
         $listParams = $list->getParams();
+        $listParams['rex_yform_manager_popup'] = 1;
 
-        $link = '<a class="yform-quick-edit rex-link-expanded" style="white-space: nowrap; font-size: 12px; text-decoration: none;" title="QuickEdit" data-id="###id###" href="index.php?page=yform/manager/data_edit&table_name=' . $listParams['table_name'] . '&rex_yform_manager_popup=1&data_id=###id###&func=edit&list=' . $list->getName() . '&quick_edit=true"><i class="fa fa-pencil"></i> QuickEdit</a>';
+        $params = [
+            'func' => 'edit',
+            'list' => $list->getName(),
+            'quick_edit' => 'true',
+            'rex_yform_manager_opener' => 1,
+            'search' => false,
+        ];
+
+        /**
+         * add csrf token -> yform >= 4
+         */
+        if(method_exists($ep->getParam('table'), 'getCSRFKey')) {
+            $params['_csrf_token'] = \rex_csrf_token::factory($ep->getParam('table')->getCSRFKey())->getValue();
+        }
+
+        $urlParams = array_merge($listParams, $params);
+
+        $link = '<a class="yform-quick-edit rex-link-expanded" style="white-space: nowrap; font-size: 12px; text-decoration: none;" title="QuickEdit" data-id="###id###" href="'.'index.php?' . http_build_query($urlParams).'&data_id=###id###"><i class="fa fa-pencil"></i> QuickEdit</a>';
         $list->addColumn('', $link, 1);
         $list->setRowAttributes(['class' => 'quick-edit-row-###id###']);
     });
