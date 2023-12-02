746bc5a2b145e3d3de95fffed419446a1ece5838
Thorben <thorben@ascky.de>
2023-12-02 14:33:45 +0100

    feat: adds YQE_IS_ALLOWED ep, fix #11 [skip ci]

--- README.md
+++ README.md
@@ -14,4 +14,12 @@ rex_extension::register('YQE_IGNORE_TABLES', function (rex_extension_point $ep) 
 });
 ```
 
+Über den EP `YQE_IS_ALLOWED` lässt sich QuickEdit auf bestimmten Seiten aktivieren. Erwartet wird `true` oder `false`. `true` bedeutet, dass QuickEdit geladen wird. Normalerweise wird QuickEdit nur auf der Seite `index.php?page=yform/manager/data_edit` geladen.
+
+```php
+rex_extension::register('YQE_IS_ALLOWED', function (rex_extension_point $ep) {
+    $ep->setSubject('index.php?page=yform/manager/data_edit' === rex_url::currentBackendPage());
+});
+```
+
 ![yqe](https://user-images.githubusercontent.com/2708231/151661458-65e1b0e4-ef53-48f1-bc46-3c55712f7494.png)
--- boot.php
+++ boot.php
@@ -1,57 +1,61 @@
 <?php
 
-$addon = rex_addon::get('yform_quick_edit');
+rex_extension::register('PACKAGES_INCLUDED', static function () {
+    $addon = rex_addon::get('yform_quick_edit');
+    $isAllowed = rex_extension::registerPoint(new rex_extension_point('YQE_IS_ALLOWED', ('index.php?page=yform/manager/data_edit' === rex_url::currentBackendPage())));
 
-if (rex::isBackend() && null !== rex::getUser() && ('index.php?page=yform/manager/data_edit' === rex_url::currentBackendPage())) {
-    rex_view::setJsProperty('yform_quick_edit_cancel', $addon->i18n('yform_quick_edit_cancel'));
-    rex_view::addCssFile($addon->getAssetsUrl('css/styles.css'));
-    rex_view::addJsFile($addon->getAssetsUrl('js/scripts.js'));
+    if (rex::isBackend() && null !== rex::getUser() && $isAllowed) {
+        rex_view::setJsProperty('yform_quick_edit_cancel', $addon->i18n('yform_quick_edit_cancel'));
+        rex_view::addCssFile($addon->getAssetsUrl('css/styles.css'));
+        rex_view::addJsFile($addon->getAssetsUrl('js/scripts.js'));
 
-    rex_extension::register('YFORM_DATA_LIST', static function (rex_extension_point $ep) {
-        /** @var rex_list $list */
-        $list = $ep->getSubject();
-        /** @var rex_yform_manager_table $table */
-        $table = $ep->getParam('table');
-        $listParams = $list->getParams();
-        /** @var array<string> $formsToIgnore */
-        $formsToIgnore = rex_extension::registerPoint(new rex_extension_point('YQE_IGNORE_TABLES', []));
+        rex_extension::register('YFORM_DATA_LIST', static function (rex_extension_point $ep) {
+            /** @var rex_list $list */
+            $list = $ep->getSubject();
+            /** @var rex_yform_manager_table $table */
+            $table = $ep->getParam('table');
+            $listParams = $list->getParams();
+            /** @var array<string> $formsToIgnore */
+            $formsToIgnore = rex_extension::registerPoint(new rex_extension_point('YQE_IGNORE_TABLES', []));
 
-        if (in_array($listParams['table_name'], $formsToIgnore, true)) {
-            return;
-        }
+            if (in_array($listParams['table_name'], $formsToIgnore, true)) {
+                return;
+            }
 
-        $listParams['rex_yform_manager_popup'] = 1;
-        $params = [
-            'func' => 'edit',
-            'list' => $list->getName(),
-            'quick_edit' => 'true',
-            'rex_yform_manager_opener' => 1,
-            'yqe' => 'true',
-        ];
-
-        /**
-         * add csrf token -> yform >= 4.
-         */
-        $params['_csrf_token'] = rex_csrf_token::factory($table->getCSRFKey())->getValue();
-
-        $urlParams = array_merge($listParams, $params);
-
-        $link = '<a class="yform-quick-edit rex-link-expanded" style="white-space: nowrap; font-size: 12px; text-decoration: none;" title="QuickEdit" data-id="###id###" href="index.php?'. http_build_query($urlParams) . '&data_id=###id###"><i class="fa fa-pencil"></i> QuickEdit</a>';
-        $list->addColumn('', $link, 1);
-        $list->setRowAttributes(['class' => 'quick-edit-row-###id###']);
-    });
-
-    if ('true' === rex_get('yqe')) {
-        rex_extension::register('YFORM_MANAGER_DATA_PAGE', static function (rex_extension_point $ep) {
-            /** @var rex_yform_manager $manager */
-            $manager = $ep->getSubject();
+            $listParams['rex_yform_manager_popup'] = 1;
+            $params = [
+                'func' => 'edit',
+                'list' => $list->getName(),
+                'quick_edit' => 'true',
+                'rex_yform_manager_opener' => 1,
+                'yqe' => 'true',
+            ];
 
             /**
-             * remove search.
+             * add csrf token -> yform >= 4.
              */
-            if (($key = array_search('search', $manager->dataPageFunctions, true)) !== false) {
-                unset($manager->dataPageFunctions[$key]);
-            }
+            $params['_csrf_token'] = rex_csrf_token::factory($table->getCSRFKey())->getValue();
+
+            $urlParams = array_merge($listParams, $params);
+
+            $link = '<a class="yform-quick-edit rex-link-expanded" style="white-space: nowrap; font-size: 12px; text-decoration: none;" title="QuickEdit" data-id="###id###" href="index.php?'. http_build_query($urlParams) . '&data_id=###id###"><i class="fa fa-pencil"></i> QuickEdit</a>';
+            $list->addColumn('', $link, 1);
+            $list->setRowAttributes(['class' => 'quick-edit-row-###id###']);
         });
+
+        if ('true' === rex_get('yqe')) {
+            rex_extension::register('YFORM_MANAGER_DATA_PAGE', static function (rex_extension_point $ep) {
+                /** @var rex_yform_manager $manager */
+                $manager = $ep->getSubject();
+
+                /**
+                 * remove search.
+                 */
+                if (($key = array_search('search', $manager->dataPageFunctions, true)) !== false) {
+                    unset($manager->dataPageFunctions[$key]);
+                }
+            });
+        }
     }
-}
+});
+
